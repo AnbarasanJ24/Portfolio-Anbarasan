fd4eb02ce53e643151fa175a087efd20dd38ff7e
Anbarasan <anbarasanj24@gmail.com>
2022-08-02 10:03:45 +0530

    Added Navbar and made it naviagte to other section of portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css';
-
+import {BiHomeHeart,BiUser,BiBookmarkHeart,BiDialpadAlt,BiMessage} from 'react-icons/bi'
 const Nav = () => {
+  const [active, setActive] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="/#" onClick={()=> setActive('#')} className={active === '#' ? 'active' : ''}><BiHomeHeart /></a>
+      <a href="/#about" onClick={()=> setActive('#about')} className={active === '#about' ? 'active' : ''}><BiUser /></a>
+      <a href="/#experience" onClick={()=> setActive('#experience')} className={active === '#experience' ? 'active' : ''}><BiBookmarkHeart /></a>
+      <a href="/#services" onClick={()=> setActive('#services')} className={active === '#services' ? 'active' : ''}><BiDialpadAlt /></a>
+      <a href="/#portfolio" onClick={()=> setActive('#portfolio')} className={active === '#portfolio' ? 'active' : ''}><BiHomeHeart /></a>
+      <a href="/#contact" onClick={()=> setActive('#contact')} className={active === '#contact' ? 'active' : ''}><BiMessage /></a>
+    </nav>
   )
 }
 
