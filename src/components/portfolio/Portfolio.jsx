7b46c3b25a251aaef4f5e4cf023a5070ef7742fe
Anbarasan <anbarasanj24@gmail.com>
2022-08-04 07:57:31 +0530

    Added initial setup for portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './portfolio.css'
+import { portfolioData } from '../../data/portfolio'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+
+        {
+          portfolioData.map(portfolio => {
+            let { id, image, title, github, demo } = portfolio;
+
+            return (
+              <article className="portfolio__item" key={id}>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-btn">
+                  <a href={github} className='btn' arget='_blank' rel="noreferrer">Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+
+
+      </div>
+    </section>
   )
 }
 
