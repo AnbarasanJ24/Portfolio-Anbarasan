e1f32f0e5c34354c1087367b72f05e0ceace8546
Anbarasan <anbarasanj24@gmail.com>
2022-08-05 07:36:45 +0530

    Modified Resume & Removed unwanted Images

--- src/components/header/HeaderBtn.jsx
+++ src/components/header/HeaderBtn.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import resume from '../../assets/cv.pdf'
+import resume from '../../assets/Anbarasan-Frontend-Engineer-3YOE.pdf'
 
 const HeaderBtn = () => {
   return (
     <div className='btn-container'>
         <button><a href={resume} download className='btn'>Download Resume</a></button>
-        <button><a href="/#" className='btn btn-primary'>Lets'talk</a></button>
+        <button><a href="/#contact" className='btn btn-primary'>Lets'talk</a></button>
     </div>
   )
 }
