e34052d77fd5922ccb27da0bc2dcacb0dbcdd473
Anbarasan <anbarasanj24@gmail.com>
2022-08-06 10:33:06 +0530

    Modifed portfolio and footer section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,19 +6,23 @@ const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Works</h5>
-      <h2>Portfolio</h2>
+      <h2>Projects</h2>
       <div className="container portfolio__container">
 
         {
           portfolioData.map(portfolio => {
-            let { id, image, title, github, demo } = portfolio;
+            let { id, image, title, tech, github,description, demo } = portfolio;
 
             return (
               <article className="portfolio__item" key={id}>
                 <div className="portfolio__item-image">
                   <img src={image} alt={title} />
                 </div>
+                <div>
                 <h3>{title}</h3>
+                <small>{tech}</small>
+                <p>{description}</p>
+                </div>
                 <div className="portfolio__item-btn">
                   <a href={github} className='btn' arget='_blank' rel="noreferrer">Github</a>
                   <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
