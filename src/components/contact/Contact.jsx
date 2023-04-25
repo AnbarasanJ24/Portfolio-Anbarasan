6532abbea52eb497925f63deebd73e918aceb455
Anbarasan <anbarasanj24@gmail.com>
2023-04-25 21:55:34 +0530

    Modifed profile as on Apr 2023

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -36,8 +36,8 @@ const Contact = () => {
           <article className='contact__option'>
             <BsWhatsapp className='.contact__option-icon' />
             <h4>WhatsApp</h4>
-            <h5>+91 9791333906</h5>
-            <a href="https://api.whatsapp.com/?send?phone=+919791333906"
+            <h5>+971 521587915</h5>
+            <a href="https://api.whatsapp.com/?send?phone=+971521587915"
               target='_blank' rel="noreferrer">Send a quick message</a>
           </article>
         </div>
--- src/components/header/HeaderBtn.jsx
+++ src/components/header/HeaderBtn.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import resume from '../../assets/Anbarasan-Frontend-Engineer-3YOE.pdf'
+import resume from '../../assets/Anbarasan-Frontend-Engineer-4YOE.pdf'
 
 const HeaderBtn = () => {
   return (
     <div className='btn-container'>
         {/* Commented for some time */}
-        {/* <button><a href={resume} download className='btn'>Download Resume</a></button> */}
+        <button><a href={resume} download className='btn'>Download Resume</a></button>
         <button><a href="/#contact" className='btn btn-primary'>Let's Talk</a></button>
     </div>
   )
